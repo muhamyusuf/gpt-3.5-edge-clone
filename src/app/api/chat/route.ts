bd8db7aa117d8cb1ec8dddceafedf5994ebb07dc
Wupxy <muhammad.122140193@student.itera.ac.id>
2023-09-23 18:59:33 +0700

    initial commit

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,24 @@
+// /api/chat
+import { Configuration, OpenAIApi } from "openai-edge";
+import { OpenAIStream, StreamingTextResponse } from "ai";
+
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(config);
+
+export async function POST(req: Request) {
+  const body = await req.json();
+  const { messages } = body;
+
+  // ask openai for the streaming chat completion
+  const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages,
+    stream: true,
+  });
+
+  const stream = OpenAIStream(response);
+  return new StreamingTextResponse(stream);
+}
